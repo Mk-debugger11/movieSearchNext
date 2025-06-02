eeb5b58525e23c34dd338a0cbd2b6dffc42d9fa8
Mukul Kumar <mukulkumarsharma540@gmail.com>
2025-06-02 23:34:01 +0530

    updated

--- src/app/movie/[movieid]/page.jsx
+++ src/app/movie/[movieid]/page.jsx
@@ -1,6 +1,5 @@
 
 import DetailedMoviePage from "@/app/components/detailMoviePage";
-
 export default async function MoviePage({ params }) {
   const movieData = await params
   return <DetailedMoviePage movieId={movieData.movieid} />;
